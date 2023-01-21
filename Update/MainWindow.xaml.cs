6d83b19de95ed87c70358384565d9e605bd8dcbe
Marko Medhat <85482481+Zer0bytte@users.noreply.github.com>
2023-01-21 21:34:49 +0200

    d

--- Update/MainWindow.xaml.cs
+++ Update/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Squirrel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Update
 {
@@ -20,9 +8,38 @@ namespace Update
     /// </summary>
     public partial class MainWindow : Window
     {
+        UpdateManager manager;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            manager = await UpdateManager.GitHubUpdateManager(@"https://github.com/Zer0bytte/UpdateCheck");
+            CurrentVersionTextBox.Text = manager.CurrentlyInstalledVersion().ToString();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var updateInfo = await manager.CheckForUpdate();
+
+            if (updateInfo.ReleasesToApply.Count > 0)
+            {
+                UpdateButton.IsEnabled = true;
+            }
+            else
+            {
+                UpdateButton.IsEnabled = false;
+            }
+        }
+
+        private async void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            await manager.UpdateApp();
+
+            MessageBox.Show("Updated succesfuly!");
+
         }
     }
 }
